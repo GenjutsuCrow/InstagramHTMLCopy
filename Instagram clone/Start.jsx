5877d6fb6d2592a66e8cdd41231d33aab97eee53
GenjutsuCrow <145786415+GenjutsuCrow@users.noreply.github.com>
2024-09-21 20:19:36 +0200

    Add files via upload

---
+++ Instagram clone/Start.jsx
@@ -0,0 +1,10 @@
+import profilepic from 'E:\V Studio\Instagram clone\armor.webp'
+
+function Card(){
+return
+    <div>
+        <img src={profilepic} alt=">profilepic"></img>
+        <h>Hello world</h>
+        <p>HERE IS THE START</p>
+</div>
+}
